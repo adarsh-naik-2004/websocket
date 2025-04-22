5ffc60948e8997654c878a59d2ad80ddcb9ba514
adarsh-naik-2004 <adarshnaik270@gmail.com>
2025-04-22 19:36:47 +0530

    consume and emit

--- src/socket.ts
+++ src/socket.ts
@@ -9,8 +9,8 @@ io.on("connection", (socket) => {
   console.log("Client connected", socket.id);
 
   socket.on("join", (data) => {
-    socket.join(String(data.tenantId));
-    socket.emit("join", { roomId: String(data.tenantId) });
+    socket.join(String(data.storeId));
+    socket.emit("join", { roomId: String(data.storeId) });
   });
 });
 
