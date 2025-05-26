74ade8249b74aa64ef50e74b0117428738202e67
adarsh-naik-2004 <adarshnaik270@gmail.com>
2025-05-26 21:43:25 +0530

    add sasl authentication

--- src/config/kafka.ts
+++ src/config/kafka.ts
@@ -1,11 +1,30 @@
-import { Consumer, EachMessagePayload, Kafka } from "kafkajs";
+import { Consumer, EachMessagePayload, Kafka, KafkaConfig } from "kafkajs";
 import { MessageBroker } from "../types/broker";
 import ws from "../socket";
+import config from "config";
 export class KafkaBroker implements MessageBroker {
   private consumer: Consumer;
 
   constructor(clientId: string, brokers: string[]) {
-    const kafka = new Kafka({ clientId, brokers });
+    let kafkaConfig: KafkaConfig = {
+      clientId,
+      brokers,
+    };
+
+    if (process.env.NODE_ENV === "production") {
+      kafkaConfig = {
+        ...kafkaConfig,
+        ssl: true,
+        connectionTimeout: 45000,
+        sasl: {
+          mechanism: "plain",
+          username: config.get("kafka.sasl.username"),
+          password: config.get("kafka.sasl.password"),
+        },
+      };
+    }
+
+    const kafka = new Kafka(kafkaConfig);
 
     this.consumer = kafka.consumer({ groupId: clientId });
   }
@@ -43,7 +62,6 @@ export class KafkaBroker implements MessageBroker {
         switch (topic) {
           case "order":
             {
-              // todo: maybe check event_type ?
               const order = JSON.parse(message.value.toString());
               ws.io.to(order.data.storeId).emit("order-update", order);
             }
